9c26f82066abc1331a8fefb2122a7bec8783af7b
TheLukeGuy <lpc@lukepchambers.net>
2015-11-21 18:16:25 -0500

    Added music (work in progress)

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import datetime
 import os.path
 import random
 import urllib2
+import subprocess
+import afplay
 
 #Creates a variable for each data file path.
 FIRST_BOOT_FILE = 'data/extra/first_boot.txt'
@@ -131,135 +133,140 @@ else:
 	print('Welcome back, {}!'.format(name))
 
 while True:
-	command = raw_input("> ")
-	if command == "help":
-		print("Available commands:")
-		print("calculator - pyCalc v2.0")
-		print("clear - clears console")
-		print("quit - quits tlgOS")
-		print("reset - resets tlgOS")
-		print("editor - pyEdit beta 1.0")
-		print("credits - tlgOS credits")
-		print("zoik - Zoik, your new best friend")
-		print("about - about your copy of tlgOS")
-		print("update - checks for tlgOS updates")
-		print("copyright - views copyright info")
-		print("web - pyWeb pre-alpha 1.0")
+	try:
+		command = raw_input("> ")
+		if command == "help":
+			print("Available commands:")
+			print("calculator - pyCalc v2.0")
+			print("clear - clears console")
+			print("quit - quits tlgOS")
+			print("reset - resets tlgOS")
+			print("editor - pyEdit beta 1.0")
+			print("credits - tlgOS credits")
+			print("zoik - Zoik, your new best friend")
+			print("about - about your copy of tlgOS")
+			print("update - checks for tlgOS updates")
+			print("copyright - views copyright info")
+			print("music - plays music you upload")
 
-	if command == "calculator":
-		print("pyCalc v2.0")
-		try:
-			v1 = int(raw_input("Please enter the first value: "))
-		except ValueError:
-			print("{} is not a number".format(v1))
-			sys.exit(1)
-		operation = raw_input("Please enter an operation: ")
-		if operation not in ["+","-","*","/"]:
-			print("{} is not a valid operation".format(operation))
-			sys.exit(1)
-		try:
-			v2 = int(raw_input("Please enter the second value: "))
-		except ValueError:
-			print("{} is not a number".format(v2))
-			sys.exit(1)
-		print("Solving problem...")
-		if operation == "+":
-			answer = int(v1+v2)
-		if operation == "-":
-			answer = int(v1-v2)
-		if operation == "*":
-			answer = int(v1*v2)
-		if operation == "/":
-			answer = int(v1/v2)
-		print("The answer to {} {} {} is {}".format(v1, operation, v2, answer))
-
-	if command == "clear":
-		print("\n" * 100)
-		print("Console cleared!")
-
-	if command == "quit":
-		print("Farewell until we meet again, {}!".format(name))
-		sys.exit(0)
-
-	if command == "reset":
-		print("This method has been removed. To reset, type quit, then, type \"python reset.py\".")
-
-	if command == "editor":
-		print("Note: pyEdit is still in beta. You CAN NOT:")
-		print("Open files")
-		print("Get a new line")
-		print("Also, all files are saved to the files folder.")
-		text = raw_input("Press enter when done > ")
-		save_as = raw_input("What would you like to save this file as? Please enter the name as well as the extention. ")
-		TEXT_FILE = 'files/{}'.format(save_as)
-		tf = open(TEXT_FILE, 'w')
-		tf.write(text)
-		tf.close()
-
-	if command == "credits":
-		print("Luke Chambers (TheLukeGuy) - creator of tlgOS project")
-		print("pyTools by Luke Chambers - programs used:")
-		print("pyCalc v2.0")
-		print("pyEdit beta 1.0")
-		print("pyWeb pre-alpha 1.0")
-		print("my dad - helped me with making this when I needed it")
-
-	if command == "zoik":
-		print("Reading data...")
-		pcf = open(PARENTAL_CONTROLS_FILE, 'r')
-		pc_pin_false = os.stat(PARENTAL_CONTROLS_FILE).st_size == 0
-		if pc_pin_false == False:
-			print("Zoik contains some language such as \"loser\" and \"kill\"")
-			pin = raw_input("Please enter your parental controls pin to access \"Zoik\": ")
-			if pin == pcf.read():
-				pcf.close()
-				privacy_clear()
-				zoik()
-			else:
-				pcf.close()
-				print("Incorrect pin")
+		if command == "calculator":
+			print("pyCalc v2.0")
+			try:
+				v1 = int(raw_input("Please enter the first value: "))
+			except ValueError:
+				print("{} is not a number".format(v1))
 				sys.exit(1)
-		else:
-			zoik()
+			operation = raw_input("Please enter an operation: ")
+			if operation not in ["+","-","*","/"]:
+				print("{} is not a valid operation".format(operation))
+				sys.exit(1)
+			try:
+				v2 = int(raw_input("Please enter the second value: "))
+			except ValueError:
+				print("{} is not a number".format(v2))
+				sys.exit(1)
+			print("Solving problem...")
+			if operation == "+":
+				answer = int(v1+v2)
+			if operation == "-":
+				answer = int(v1-v2)
+			if operation == "*":
+				answer = int(v1*v2)
+			if operation == "/":
+				answer = int(v1/v2)
+			print("The answer to {} {} {} is {}".format(v1, operation, v2, answer))
 
-	if command == "about":
-		print("tlgOS Candy Cane pre-alpha 1.1 build 2 running on",os.uname())
-		print("Written in the Python programming language")
-		print("Coded in the Atom and TextWrangler text editors")
+		if command == "clear":
+			print("\n" * 100)
+			print("Console cleared!")
 
-	if command == "update":
-		current_version = "tlgOS Candy Cane pre-alpha 1.1 build 2"
-		for line in urllib2.urlopen(TARGET_URL):
-			version = line
-		print("The latest version of tlgOS is {}".format(version))
-		print("You are running {}".format(current_version))
+		if command == "quit":
+			print("Farewell until we meet again, {}!".format(name))
+			sys.exit(0)
 
-	if command == "copyright":
-		print("Licensed under the MIT license")
-		print("The MIT License (MIT)")
+		if command == "reset":
+			print("This method has been removed. To reset, type quit, then, type \"python reset.py\".")
 
-		print("Copyright (c) 2015 Luke Chambers")
+		if command == "editor":
+			print("Note: pyEdit is still in beta. You CAN NOT:")
+			print("Open files")
+			print("Get a new line")
+			print("Also, all files are saved to the files folder.")
+			text = raw_input("Press enter when done > ")
+			save_as = raw_input("What would you like to save this file as? Please enter the name as well as the extention. ")
+			TEXT_FILE = 'files/{}'.format(save_as)
+			tf = open(TEXT_FILE, 'w')
+			tf.write(text)
+			tf.close()
 
-		print("Permission is hereby granted, free of charge, to any person obtaining a copy")
-		print("of this software and associated documentation files (the \"Software\"), to deal")
-		print("in the Software without restriction, including without limitation the rights")
-		print("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell")
-		print("copies of the Software, and to permit persons to whom the Software is")
-		print("furnished to do so, subject to the following conditions")
+		if command == "credits":
+			print("Luke Chambers (TheLukeGuy) - creator of tlgOS project")
+			print("pyTools by Luke Chambers - programs used:")
+			print("pyCalc v2.0")
+			print("pyEdit beta 1.0")
+			print("pyWeb pre-alpha 1.0")
+			print("my dad - helped me with making this when I needed it")
 
-		print("The above copyright notice and this permission notice shall be included in all")
-		print("copies or substantial portions of the Software.")
+		if command == "zoik":
+			print("Reading data...")
+			pcf = open(PARENTAL_CONTROLS_FILE, 'r')
+			pc_pin_false = os.stat(PARENTAL_CONTROLS_FILE).st_size == 0
+			if pc_pin_false == False:
+				print("Zoik contains some language such as \"loser\" and \"kill\"")
+				pin = raw_input("Please enter your parental controls pin to access \"Zoik\": ")
+				if pin == pcf.read():
+					pcf.close()
+					privacy_clear()
+					zoik()
+				else:
+					pcf.close()
+					print("Incorrect pin")
+					sys.exit(1)
+			else:
+				zoik()
 
-		print("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR")
-		print("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,")
-		print("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE")
-		print("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER")
-		print("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,")
-		print("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE")
-		print("SOFTWARE.")
+		if command == "about":
+			print("tlgOS Candy Cane pre-alpha 1.1 build 2 running on",os.uname())
+			print("Written in the Python programming language")
+			print("Coded in the Atom and TextWrangler text editors")
 
-	if command == "web":
-		print("pyWeb only displays text on a web page at the moment")
-		USER_URL = raw_input("Please enter the URL of the page (http(s)://domain.topleveldomain(/page.html)): ")
-		for line in urllib2.urlopen(USER_URL):
-			print(line)
+		if command == "update":
+			current_version = "tlgOS Candy Cane pre-alpha 1.1 build 2"
+			for line in urllib2.urlopen(TARGET_URL):
+				version = line
+			print("The latest version of tlgOS is {}".format(version))
+			print("You are running {}".format(current_version))
+
+		if command == "copyright":
+			print("Licensed under the MIT license")
+			print("The MIT License (MIT)")
+
+			print("Copyright (c) 2015 Luke Chambers")
+
+			print("Permission is hereby granted, free of charge, to any person obtaining a copy")
+			print("of this software and associated documentation files (the \"Software\"), to deal")
+			print("in the Software without restriction, including without limitation the rights")
+			print("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell")
+			print("copies of the Software, and to permit persons to whom the Software is")
+			print("furnished to do so, subject to the following conditions")
+
+			print("The above copyright notice and this permission notice shall be included in all")
+			print("copies or substantial portions of the Software.")
+
+			print("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR")
+			print("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,")
+			print("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE")
+			print("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER")
+			print("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,")
+			print("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE")
+			print("SOFTWARE.")
+
+		if command == "music":
+			print("To add music, locate to the files folder and place your music there.")
+			MUSIC_FILE = raw_input("What is the song saved as? ex. radioactive.mp3 ")
+			print("Use ^C to stop playing music")
+			afplay.afplay("files/{}".format(MUSIC_FILE))
+
+	except KeyboardInterrupt:
+		print("Please use \"quit\" to exit")
+		continue
